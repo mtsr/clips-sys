c21757b81f0240e6b466cf356c57c6af0f6dbf0f
Jonas Matser <jonas@jonasmatser.nl>
2018-12-16 21:09:39 +0100

    Build CLIPS in out_dir

--- build.rs
+++ build.rs
@@ -1,27 +1,51 @@
 extern crate bindgen;
 
 use std::env;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
   let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
   
-  if !Path::new("CLIPS/.git").exists() {
+  // clone or pull
+  if !out_dir.join("CLIPS/.git").exists() {
     let _ = Command::new("git")
-      .args(&["submodule", "update", "--init"])
+      .current_dir(&out_dir)
+      .args(&["clone", "https://github.com/mtsr/CLIPS.git"])
+      .status();
+  } else {
+    let _ = Command::new("git")
+      .current_dir(&out_dir.join("CLIPS"))
+      .args(&["pull"])
       .status();
   }
 
-  if !Path::new("CLIPS/core/clips").exists() {
-    let _ = Command::new("make")
-      // .arg("debug")
-      .current_dir("CLIPS/core")
-      .status();
+  // checkout the right branch
+  let _ = Command::new("git")
+    .current_dir(&out_dir.join("CLIPS"))
+    .args(&["checkout", "64x"])
+    .status();
+
+  // prepare make command
+  let mut cmd = Command::new("make");
+
+  // Conditionally make debug build
+  if env::var("DEBUG").is_ok() {
+    cmd.arg("debug");
   }
+  
+  // and run it
+  let _ = cmd
+  .current_dir(&out_dir.join("CLIPS/core"))
+  .status();
+
+  // copy wrapper
+  let _ = Command::new("cp")
+    .args(&["wrapper.h", &format!("{}", out_dir.display())])
+    .status();
 
   println!("cargo:rustc-link-lib=static=clips");
-  println!("cargo:rustc-link-search=./CLIPS/core/");
+  println!("cargo:rustc-link-search={}", &out_dir.join("CLIPS/core").display());
 
   let out_path = out_dir.join("bindings.rs");
 
@@ -32,7 +56,8 @@ fn main() {
     let bindings = bindgen::Builder::default()
           // The input header we would like to generate
           // bindings for.
-          .header("wrapper.h")
+          // use the copy in out_dir
+          .header(format!("{}", out_dir.join("wrapper.h").display()))
           .derive_debug(true)
           .impl_debug(true)
           .derive_default(true)
