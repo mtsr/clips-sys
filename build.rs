ad3b262dc366d037a09733a2ab0309ae710170f9
Jonas Matser <jonas@jonasmatser.nl>
2018-12-30 20:26:14 +0100

    Use git submodule + cc instead of commands

--- build.rs
+++ build.rs
@@ -1,52 +1,41 @@
 extern crate bindgen;
+extern crate cc;
 
 use std::env;
 use std::path::PathBuf;
-use std::process::Command;
 
 fn main() {
   let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+  let clips_core_dir = PathBuf::from("CLIPS/core");
+
+  let files = 
+      std::fs::read_dir(&clips_core_dir)
+      .expect("CLIPS core dir not found")
+      .map(|entry| entry.expect("not a valid entry"))
+      .filter(|entry| {
+        !entry.file_type().expect("not a valid filetype").is_dir() 
+      })
+      .map(|entry| PathBuf::from(entry.path()))
+      .filter(|path| match path.extension() {
+        None => false,
+        Some(extension) => extension == "c",
+      })
+      // TODO #define DEVELOPER 1 if feature is set
+      .filter(|path| cfg!(feature = "developer") || !path.ends_with("developr.c"));
+
+  eprintln!("{:?}", files);
+
+  cc::Build::new()
+    .files(files)
+    .include(&clips_core_dir)
+    // .warnings(false)
+    .out_dir(&out_dir)
+    // Exclude current CLIPS compile warnings
+    .flag("-Wno-unused-parameter")
+    .flag("-Wno-missing-field-initializers")
+    .compile("clips");
   
-  // clone or pull
-  if !out_dir.join("CLIPS/.git").exists() {
-    let _ = Command::new("git")
-      .current_dir(&out_dir)
-      .args(&["clone", "https://github.com/mtsr/CLIPS.git"])
-      .status();
-  } else {
-    let _ = Command::new("git")
-      .current_dir(&out_dir.join("CLIPS"))
-      .args(&["pull"])
-      .status();
-  }
-
-  // checkout the right branch
-  let _ = Command::new("git")
-    .current_dir(&out_dir.join("CLIPS"))
-    .args(&["checkout", "64x"])
-    .status();
-
-  if env::var("DEBUG").is_ok() {
-    let _ = Command::new("sed").current_dir(&out_dir.join("CLIPS")).args(&["-i", "''", "s/#define DEVELOPER 0/#define DEVELOPER 1/","core/setup.h"]).status();
-  }
-
-  // prepare make command
-  let mut cmd = Command::new("make");
-
-  // Conditionally make debug build
-  if env::var("DEBUG").is_ok() {
-    cmd.arg("debug");
-  }
-  
-  // and run it
-  let _ = cmd
-  .current_dir(&out_dir.join("CLIPS/core"))
-  .status();
-
-  // copy wrapper
-  let _ = Command::new("cp")
-    .args(&["wrapper.h", &format!("{}", out_dir.display())])
-    .status();
+  let _res = std::fs::copy(PathBuf::from("wrapper.h"), &out_dir.join("wrapper.h"));
 
   println!("cargo:rustc-link-lib=static=clips");
   println!("cargo:rustc-link-search={}", &out_dir.join("CLIPS/core").display());
@@ -61,7 +50,7 @@ fn main() {
           // The input header we would like to generate
           // bindings for.
           // use the copy in out_dir
-          .header(format!("{}", out_dir.join("wrapper.h").display()))
+          .header("CLIPS/core/clips.h")
           .derive_debug(true)
           .impl_debug(true)
           .derive_default(true)
